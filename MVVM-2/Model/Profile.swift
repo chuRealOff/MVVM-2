3791a115952808c9e0ffa7933642d2405f4fe0f4
4u.real.off <dm.churilov@gmail.com>
2022-11-15 21:43:31 +0300

    Initial Commit

---
+++ MVVM-2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by CHURILOV DMITRIY on 14.11.2022.
+//
+
+import Foundation
+
+struct Profile {
+    let name: String
+    let secondName: String
+    let age: Int
+}
---
+++ MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by CHURILOV DMITRIY on 14.11.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+}
